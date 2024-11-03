bd2b7efcd187315588a6dbe927d78404715fd920
Maksim Kozhin <a.zurnaaaa@gmail.com>
2024-11-03 13:34:21 +0300

    -

--- bot/core/scheduler/scheduled_events.py
+++ bot/core/scheduler/scheduled_events.py
@@ -8,7 +8,7 @@ from aiogram.exceptions import TelegramForbiddenError
 import bot.core.scheduler.text as text
 from bot.core.config import settings
 from bot.core.models import db_helper
-from bot.main_menu.keyboard import main_menu_actions_kb_builder
+from bot.main_menu.keyboard import main_menu_kb_builder
 from bot.movie.random_movie.keyboard import trailers_kb_builder
 from bot.movie.random_movie.utils import get_random_movie
 from bot.weather.todays_weather.keyboard import weather_today_result_kb_builder
@@ -30,7 +30,7 @@ async def good_morning(bot: Bot) -> None:
             await bot.send_sticker(
                 chat_id=user.tg_id,
                 sticker=choice(text.good_morning_stickers),
-                reply_markup=main_menu_actions_kb_builder(),
+                reply_markup=main_menu_kb_builder(as_edit=False),
             )
             if user.city:
                 async with aiohttp.ClientSession() as http_session:
@@ -144,7 +144,7 @@ async def good_vacation(bot: Bot) -> None:
             await bot.send_sticker(
                 chat_id=user.tg_id,
                 sticker=choice(text.good_morning_stickers),
-                reply_markup=main_menu_actions_kb_builder(),
+                reply_markup=main_menu_kb_builder(as_edit=False),
             )
             if user.city:
                 async with aiohttp.ClientSession() as http_session:
@@ -174,7 +174,7 @@ async def good_vacation(bot: Bot) -> None:
 
 async def happy_ny(bot: Bot) -> None:
     """Send congratulation scheduled notification to users"""
-    ny_picture_path = Path(__file__).parent / "HappyNY.jpg"
+    ny_picture_path = Path(__file__).parent / "happyNY.png"
     async with db_helper.get_session() as session:
         users = await get_users(session=session)
     for user in users:
@@ -183,9 +183,9 @@ async def happy_ny(bot: Bot) -> None:
             await bot.send_photo(
                 chat_id=user.tg_id,
                 photo=ny_picture,
-                caption=f"{user.tg_name},с Новым Годом тебя!!!❤️☃️❄️\n"
+                caption=f"{user.tg_name}, с Новым Годом тебя!!!❤️☃️❄️\n"
                 f"Пусть этот год будет переполнен только приятными событиями!",
-                reply_markup=main_menu_actions_kb_builder(),
+                reply_markup=main_menu_kb_builder(as_edit=False),
             )
         except TelegramForbiddenError:
             async with db_helper.get_session() as session:
